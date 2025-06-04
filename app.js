b9b163010a3572ec06201ff500e4274ef4df3783
Nick Yaremovich <nickyaremovich@gmail.com>
2025-06-04 13:12:55 -0600

    Refactor grid creation logic and update button ID in HTML

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-let gridSize = 16;
+const container =  document.getElementById('grid-container');
 
-const button = document.querySelector('button').addEventListener('click', () => {
-    const userInput = prompt("Pick a number between 2 and 100.");
-    if (userInput < 2 || userInput > 100)
-        alert("Error. Choose a valid number.")
-    else {
-        userInput = gridSize
-    }
-
-});
-
-
-
-const container = document.getElementById('grid-container');
-
-for (let i = 0; i < 256; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add("grid-item");
-    gridItem.addEventListener("mouseover", () => {
-    gridItem.style.backgroundColor = "black";
-});
-    container.appendChild(gridItem);
+for (let i = 0; i < 16; i++) {
+    const div = document.createElement('div');
+    div.classList.add("grid-item");
+    container.appendChild(div);
+    
 };
-
