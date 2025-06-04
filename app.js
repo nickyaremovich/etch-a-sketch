f4f4384a8c3192813543b5c73fc1ea2de986b75a
Nick Yaremovich <nickyaremovich@gmail.com>
2025-06-04 12:26:38 -0600

    nest event listener in creation loop

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const container =  document.getElementById('grid-container');
 for (let i = 0; i < 256; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add("grid-item");
+    gridItem.addEventListener("mouseover", () => {
+    gridItem.style.backgroundColor = "black";
+});
     container.appendChild(gridItem);
 };
 
-gridItem.addEventListener("mouseover", () => {
-    gridItem.style.backgroundColor = "black";
-});
