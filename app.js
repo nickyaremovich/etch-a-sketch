bc4941213f8313de11a77cc6e00eb823eac80bc7
Nick Yaremovich <nickyaremovich@gmail.com>
2025-06-04 12:11:45 -0600

    Update grid item count to 256 and fix background color in styles.css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const container =  document.getElementById('grid-container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add("grid-item");
-    container.appendChild(gridI);
+    container.appendChild(gridItem);
 };
