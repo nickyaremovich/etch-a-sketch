0d90cd55fd176e8851a73ac12bfcc7c47719985b
Nick Yaremovich <nickyaremovich@gmail.com>
2025-06-04 12:36:01 -0600

    Add button event listener for grid size input

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const container =  document.getElementById('grid-container');
+let gridSize = 16;
+
+const button = document.querySelector('button').addEventListener('click', () => {
+    const userInput = prompt("Pick a number between 2 and 100.");
+    if (userInput < 2 || userInput > 100)
+        alert("Error. Choose a valid number.")
+    else {
+        userInput = gridSize
+    }
+
+});
+
+
+
+const container = document.getElementById('grid-container');
 
 for (let i = 0; i < 256; i++) {
     const gridItem = document.createElement('div');
